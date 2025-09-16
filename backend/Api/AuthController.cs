5e19353c735f2b118e510c9922ccf004a29acb62
andriusandriusandrius <and.zurba@gmail.com>
2025-09-16 20:53:46 +0300

    declare auth controller class

--- backend/Api/AuthController.cs
+++ backend/Api/AuthController.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    public AuthController(IAuthService authService)
+    { 
+        _authService = authService
+    }
+}
