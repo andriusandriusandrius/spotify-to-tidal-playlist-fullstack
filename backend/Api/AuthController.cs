82b9ae613d3eb3651e331540be826e8af778fcc9
andriusandriusandrius <and.zurba@gmail.com>
2025-09-16 20:56:08 +0300

    add namespace

--- backend/Api/AuthController.cs
+++ backend/Api/AuthController.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
-[ApiController]
-[Route("api/[controller]")]
-public class AuthController : ControllerBase
-{
-    private readonly IAuthService _authService;
-    public AuthController(IAuthService authService)
-    { 
-        _authService = authService
+namespace backend.Api {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService
+        }
     }
 }
