10e189c60c9a1e560c02e56bae36f63e7c43a9d0
andriusandriusandrius <and.zurba@gmail.com>
2025-09-18 11:37:30 +0300

    swagger support

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Swagger UI at root: http://localhost:5000/
+    });
+}
+
 app.UseSession();
-app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
