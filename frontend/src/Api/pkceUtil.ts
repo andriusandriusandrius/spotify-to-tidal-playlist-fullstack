db1700d2464cced85e70819805791fa3c88c0028
andriusandriusandrius <and.zurba@gmail.com>
2025-09-19 17:13:59 +0300

    code challange and tanstack query installation

--- frontend/src/Api/pkceUtil.ts
+++ frontend/src/Api/pkceUtil.ts
@@ -1,5 +1,13 @@
+import base64url from 'base64url';
+import { sha256 } from 'js-sha256';
+
 export const codeVerifier = () => {
   const array = new Uint32Array(56 / 2);
   window.crypto.getRandomValues(array);
   return Array.from(array, (elm) => '0' + elm.toString(16).substring(-2)).join('');
+};
+
+export const generateCodeChallange = (verifier: string) => {
+  const hash = sha256.arrayBuffer(verifier);
+  return base64url(Buffer.from(hash));
 };
