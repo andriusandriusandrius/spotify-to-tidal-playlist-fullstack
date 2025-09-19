f5445eb1e8ba92d3cacbaa3160d0dedd2a724288
andriusandriusandrius <and.zurba@gmail.com>
2025-09-19 20:42:27 +0300

    change env names

--- frontend/src/Api/env.ts
+++ frontend/src/Api/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-  TIDAL_LINK: import.meta.env.TIDAL_LINK || '',
-  TIDAL_CLIENT_ID: import.meta.env.TIDAL_CLIENT_ID || '',
-  TIDAL_CLIENT_SECRET: import.meta.env.TIDAL_CLIENT_SECRET || '',
-  FRONTEND_REDIR: import.meta.env.FRONTEND_REDIR || '',
+  TIDAL_LINK: import.meta.env.VITE_TIDAL_LINK || '',
+  TIDAL_CLIENT_ID: import.meta.env.VITE_TIDAL_CLIENT_ID || '',
+  TIDAL_CLIENT_SECRET: import.meta.env.VITE_TIDAL_CLIENT_SECRET || '',
+  FRONTEND_REDIR: import.meta.env.VITE_FRONTEND_REDIR || '',
 };
